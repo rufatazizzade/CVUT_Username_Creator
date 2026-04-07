ca002ceaff7eb49712c040227a4aec3607a61076
Rufat Azizzade <azizzruf@fit.cvut.cz>
2026-04-08 01:40:23 +0200

    deleted trash files

--- target/debug/deps/thirdlesson.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\deps\thirdlesson.d: src\main.rs
-
-C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\deps\thirdlesson.exe: src\main.rs
-
-src\main.rs:
--- target/debug/thirdlesson.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\target\debug\thirdlesson.exe: C:\Users\Admin\Desktop\rust\01\3th'\thirdlesson\src\main.rs
